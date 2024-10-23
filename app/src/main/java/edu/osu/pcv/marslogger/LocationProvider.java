32f73f50af39b586a68a590cb32346e82d27b624
Jianzhu Huai <huai.3@osu.edu>
2024-10-23 17:21:53 +0800

    switch between google api FusedLocation and LocationManager

--- app/src/main/java/edu/osu/pcv/marslogger/LocationProvider.java
+++ app/src/main/java/edu/osu/pcv/marslogger/LocationProvider.java
@@ -4,6 +4,9 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.HandlerThread;
 import android.util.Log;
 
@@ -11,37 +14,34 @@ import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.util.Consumer;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 
-import java.util.LinkedHashMap;
 
 public class LocationProvider {
 
     private final Context context;
-
-    // background thread that listens for gps changes
     private HandlerThread locationThread;
-    LocationCallback locationCallback;
-    FusedLocationProviderClient fusedLocationClient;
+    private LocationCallback locationCallback;
+    private FusedLocationProviderClient fusedLocationClient;
+    private LocationManager locationManager;
 
-    // for handling gps updates
-    private static final int DEFAULT_UPDATE_INTERVAL = 1;
-    private static final int FAST_UPDATE_INTERVAL = 1;
+    private static final int DEFAULT_UPDATE_INTERVAL = 1000; // Interval in milliseconds
+    private static final int FAST_UPDATE_INTERVAL = 500; // Fast interval in milliseconds
 
-    // to consume locations other than the latest one
     private final Consumer<Location> consumer;
+    private Location currLocation;
 
     public LocationProvider(Context context, Consumer<Location> consumer) {
         this.context = context;
+        this.locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
         this.consumer = consumer;
     }
-
-    // the most recent location
-    private Location currLocation;
 
     public Location getCurrLocation() {
         return currLocation;
@@ -50,47 +50,115 @@ public class LocationProvider {
     public void createLocationListener() {
         locationThread = new HandlerThread("locationThread");
         locationThread.start();
+
         LocationRequest locationRequest = LocationRequest.create();
-        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
-        locationRequest.setFastestInterval(1000 * FAST_UPDATE_INTERVAL);
+        locationRequest.setInterval(DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FAST_UPDATE_INTERVAL);
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
-
-        // check if you have permission before trying to get the location
-        int fineLocationPermissionCode = ActivityCompat.checkSelfPermission(
-                context, Manifest.permission.ACCESS_FINE_LOCATION
-        );
-        int coarseLocationPermissionCode = ActivityCompat.checkSelfPermission(
-                context, Manifest.permission.ACCESS_COARSE_LOCATION
-        );
-        if (fineLocationPermissionCode == PackageManager.PERMISSION_GRANTED
-                && coarseLocationPermissionCode == PackageManager.PERMISSION_GRANTED) {
-            fusedLocationClient.getLastLocation()
-                    .addOnSuccessListener(location -> currLocation = location);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.e("LocationService", "Location permission not granted!");
+            return; // Exit early if permission isn't granted
         }
 
-        // triggered whenever the location is updated
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(@NonNull LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                if (consumer != null) {
-                    Log.d("something", "locations " + locationResult.getLocations().size());
-                    for (Location location : locationResult.getLocations()) {
+        GoogleApiAvailability googleApiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = googleApiAvailability.isGooglePlayServicesAvailable(context);
+
+        if (resultCode == ConnectionResult.SUCCESS) {
+            // Google Play Services is available, use FusedLocationProviderClient
+            Log.d("LocationService", "Google Play Services available, using FusedLocationProviderClient");
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(location -> {
+                if (location != null) {
+                    currLocation = location;
+                    if (consumer != null) {
                         consumer.accept(location);
                     }
+                } else {
+                    Log.w("LocationService", "No last known location available");
                 }
-                currLocation = locationResult.getLastLocation();
-            }
-        };
-        fusedLocationClient.requestLocationUpdates(
-                locationRequest, locationCallback, locationThread.getLooper()
-        );
+            });
+
+            // Setup the callback for continuous location updates
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(@NonNull LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    for (Location location : locationResult.getLocations()) {
+                        if (location != null && consumer != null) {
+                            consumer.accept(location);
+                            currLocation = location;
+                            Log.d("LocationService", "New location: " + location.toString());
+                        }
+                    }
+                }
+            };
+
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, locationThread.getLooper());
+
+        } else {
+            Log.w("LocationService", "Google Play Services unavailable, falling back to LocationManager");
+            fallbackToLocationManager();
+        }
     }
 
+    private void fallbackToLocationManager() {
+        try {
+            boolean isGPSEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                Log.w("LocationService", "No GPS or Network provider is enabled!");
+                return;
+            }
+
+            // Use the best available provider (GPS if available, otherwise network)
+            String provider = isGPSEnabled ? LocationManager.GPS_PROVIDER : LocationManager.NETWORK_PROVIDER;
+
+            locationManager.requestLocationUpdates(provider, 1000, 0.f, locationListener);
+
+            Location lastKnownLocation = locationManager.getLastKnownLocation(provider);
+            if (lastKnownLocation != null) {
+                currLocation = lastKnownLocation;
+                if (consumer != null)
+                    consumer.accept(lastKnownLocation);
+                Log.d("LocationService", "Fallback last known location: " + lastKnownLocation);
+            }
+
+        } catch (SecurityException e) {
+            Log.e("LocationService", "Location permission not granted for LocationManager.", e);
+        }
+    }
+
+    private final LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if (consumer != null && location != null) {
+                consumer.accept(location);
+            }
+            currLocation = location;
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) { }
+
+        @Override
+        public void onProviderEnabled(String provider) { }
+
+        @Override
+        public void onProviderDisabled(String provider) { }
+    };
+
     public void quitThread() {
-        fusedLocationClient.removeLocationUpdates(locationCallback);
-        locationThread.quitSafely();
+        if (fusedLocationClient != null && locationCallback != null) {
+            fusedLocationClient.removeLocationUpdates(locationCallback);
+        }
+
+        if (locationThread != null) {
+            locationThread.quitSafely();
+        }
     }
 }
+
